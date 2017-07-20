28e974ba801e785eb904005190ddbdc2f3ec6c5e
Agnieszka Czyżak <agnieszka.czyzak@polidea.com>
2017-07-20 15:38:29 +0200

    final commit

--- Tinder/MainView.swift
+++ Tinder/MainView.swift
@@ -13,8 +13,21 @@ import UIKit
 class MainView: UIView {
     @IBOutlet weak var CatView: UIImageView!
     
+    @IBOutlet weak var Image_profile: UIView!
+    @IBOutlet weak var leftView: UIView!
+    @IBOutlet weak var rightSwitch: UIView!
+    
     override func awakeFromNib() {
-        CatView.accessibilityLabel = "some custom label"
+        CatView.accessibilityLabel = "Photo. Adult Cat. Serious. Portrait. Sharp"
+        CatView.layer.cornerRadius = 10.0
+        leftView.layer.cornerRadius = 15.0
+        rightSwitch.layer.cornerRadius = 15.0
+        rightSwitch.layer.borderColor = UIColor(red: 218.0/255.0, green: 223.0/255.0, blue: 230.0/255.0, alpha: 1.0).cgColor
+        //rightSwitch.layer.borderColor = UIColor.red.cgColor
+
+        rightSwitch.layer.borderWidth = 1.0
+        Image_profile.layer.cornerRadius = 10.0
+        
     }
     
     required init?(coder aDecoder: NSCoder) {
